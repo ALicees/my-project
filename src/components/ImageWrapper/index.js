68504d6f9b143766ea6c38c7bb263566ed5c5e95
miss zhang <13593881971@163.com>
2018-07-05 09:43:14 +0800

    my

---
+++ src/components/ImageWrapper/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default ({src,desc,style}) => {
+    <div style="{style}" classname="{styles.imageWrapper}">
+        <img classname="{styles.img}" src="{src}" alt="{desc}" />
+    </div>
+};
---
+++ src/routes/Index/Main.js
@@ -0,0 +1,10 @@
+import React, { PureComponent, Fragment } from 'react';
+
+
+export default class StepForm extends PureComponent {
+  render() {
+    return(
+      <h1>成功就是这么简单</h1>
+    )
+  }
+}
